4e4ca841e903adc3cd418eef7b8c69ebf6b3978d
Swapnil345 <swapnilkm03@gmail.com>
2025-08-30 14:35:15 +0530

    Time: 10 ms (75.32%) | Memory: 42.9 MB (14.27%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,24 +1,49 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
+        // if(s.length()!=t.length()){
+        //     return false;
+        // }
+        // HashMap<Character,Integer> map1=new HashMap<>();
+        // HashMap<Character,Integer> map2=new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     int temp=map1.getOrDefault(s.charAt(i),0);
+        //     map1.put(s.charAt(i),temp+1);
+        //     temp=map2.getOrDefault(t.charAt(i),0);
+        //     map2.put(t.charAt(i),temp+1);
+        // }
+        // // for(int i=0;i<t.length();i++){
+        // //     int temp=map.getOtDefault(t.charAt(i),0);
+        // //     map.put(t.charAt(i),temp+1);
+        // // }
+        // for(int i=0;i<s.length();i++){
+        //     if(map1.get(s.charAt(i))!=map2.get(t.charAt(i))){
+        //         return false;
+        //     }
+        // }
+        // return true;
 
-        HashMap<Character, Character> map = new HashMap<>();
-        HashSet<Character> mappedChars = new HashSet<>();
+        if(s.length()!=t.length()){
+            return false;
+        }
 
-        for (int i = 0; i < s.length(); i++) {
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
+        Map<Character,Character> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char original=s.charAt(i);
+            char replacement=t.charAt(i);
 
-            if (map.containsKey(c1)) {
-                // If current character already mapped, check if it matches
-                if (map.get(c1) != c2) return false;
-            } else {
-                // If c2 already mapped to another character, return false
-                if (mappedChars.contains(c2)) return false;
-
-                // Create new mapping
-                map.put(c1, c2);
-                mappedChars.add(c2);
+            if(!map.containsKey(original)){
+                if(!map.containsValue(replacement))
+                {map.put(original,replacement);}
+            
+            else{
+                return false;
+            }
+            }
+            else{
+                char mappedCharacter=map.get(original);
+                if(mappedCharacter!=replacement){
+                    return false;
+                }
             }
         }
         return true;
