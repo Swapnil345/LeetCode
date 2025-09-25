e2bdc6ae4e8d01a1df656d9731f79c65c4cd61cb
Swapnil345 <swapnilkm03@gmail.com>
2025-09-25 22:34:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (73.28%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,30 @@
+// class Solution {
+//     public int hammingWeight(int n) {
+//         int bin=0;
+//         while(n>0){
+//             int temp=n%2;
+//             bin=(10*bin)+temp;
+//             n=n/2;
+//         }
+//         int count=0;
+//         while(bin>0){
+//             int temp=bin%10;
+//             if(temp==1){
+//                 count++;
+//             }
+//             bin=bin/10;
+//         }
+//         return count;
+//     }
+// }
+
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n!=0){
+            count+=n&1;
+            n>>>=1;
+        }
+        return count;
+    }
+}
