3be81b1f1fa8647fd26f83341d878748200603cb
Swapnil345 <swapnilkm03@gmail.com>
2025-09-28 10:58:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (63.94%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,9 +1,9 @@
 class Solution {
     public int reverseBits(int n) {
-        int result = 0;
-        for (int i = 0; i < 32; i++) {
-            result = (result << 1) | (n & 1);
-            n >>>= 1;
+        int result=0;
+        for(int i=0;i<32;i++){
+            result=(result<<1)|(n&1);
+            n>>>=1;
         }
         return result;
     }
