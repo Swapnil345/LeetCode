de913cd7d92f82619d4dbdc7b7f9bfea30241595
Swapnil345 <swapnilkm03@gmail.com>
2025-10-01 22:56:26 +0530

    Time: 5 ms (38.72%) | Memory: 46.1 MB (7.54%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,16 +1,27 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-    for (int i = 0; i < 32; i++) {
-        int bitCount = 0;
-        for (int num : nums) {
-            bitCount += ((num >> i) & 1);
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int temp=map.getOrDefault(nums[i],0);
+            map.put(nums[i],temp+1);
         }
-        if (bitCount % 3 != 0) {
-            result |= (1 << i);
+        for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i])==1){
+                return nums[i];
+            }
         }
-    }
-    // Handle negative numbers.
-    return result;
+        return -1;
+    //     int result = 0;
+    // for (int i = 0; i < 32; i++) {
+    //     int bitCount = 0;
+    //     for (int num : nums) {
+    //         bitCount += ((num >> i) & 1);
+    //     }
+    //     if (bitCount % 3 != 0) {
+    //         result |= (1 << i);
+    //     }
+    // }
+    // // Handle negative numbers.
+    // return result;
     }
 }
