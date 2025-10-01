c3aa00d52210481aa7f2a663a4f637ed1d05525a
Swapnil345 <swapnilkm03@gmail.com>
2025-10-02 00:17:35 +0530

    Time: 60 ms (46.11%) | Memory: 64.4 MB (69.93%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,26 +1,11 @@
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-        int n=nums.length;
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-        list.add(nums[i]);
-    }
-        for(int i=0;i<n;i++){
-            list.add(reverse(list.get(i)));
-        }
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<list.size();i++){
-            int temp=map.getOrDefault(list.get(i),0);
-            map.put(list.get(i),temp+1);
-        }
-        int count =0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() >= 1) {
-                 count++; 
-            }
-
-        }
-        return count;
+       HashSet<Integer> set = new HashSet<>();
+       for(int i=0;i<nums.length;i++){
+        set.add(nums[i]);
+        set.add(reverse(nums[i]));
+       }
+       return set.size();
     }
     public int reverse(int x){
         int reverse=0;
@@ -32,3 +17,24 @@ class Solution {
         return reverse;
     }
 }
+//  int n=nums.length;
+//         ArrayList<Integer> list = new ArrayList<>();
+//         for (int i = 0; i < n; i++) {
+//         list.add(nums[i]);
+//     }
+//         for(int i=0;i<n;i++){
+//             list.add(reverse(list.get(i)));
+//         }
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         for(int i=0;i<list.size();i++){
+//             int temp=map.getOrDefault(list.get(i),0);
+//             map.put(list.get(i),temp+1);
+//         }
+//         int count =0;
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             if (entry.getValue() >= 1) {
+//                  count++; 
+//             }
+
+//         }
+//         return count;
