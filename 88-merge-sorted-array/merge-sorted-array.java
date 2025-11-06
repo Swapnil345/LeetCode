762d9f95b23bb5c004ee0e003e3ec6bd6ec954a1
Swapnil Kumar <swapnilkm03@gmail.com>
2025-11-06 11:19:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (5.39%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -18,7 +18,7 @@ class Solution {
         int p2=n-1;
         int p=m+n-1;
         while(p1>=0&&p2>=0){
-            if(nums1[p1]>nums2[p2]){
+            if(nums1[p1]>=nums2[p2]){
                 nums1[p]=nums1[p1];
                 p--;
                 p1--;
