df8f5cc33605532c5090b5122d2bf3f34bd68ca0
Swapnil Kumar <swapnilkm03@gmail.com>
2025-11-07 11:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (5.26%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,43 +1,16 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        // int i=0;
-        // int count=0;
-        // while(i<nums.length-count){
-        //     if(nums[i]==val){
-        //         int temp=nums[i];
-        //         nums[i]=nums[nums.length-1-count];
-        //         nums[nums.length-1-count]=temp;
-        //         count++;
-        //     }
-        //     else{
-        //         i++;
-        //     }
-        // }
-        // return nums.length-count;
-        // int i=0;
-        // int j=nums.length-1;
-        // while(i<j){
-        //     if(nums[i]==val){
-        //         int temp=nums[i];
-        //         nums[i]=nums[j];
-        //         nums[j]=temp;
-        //         j--;
-        //     }
-        //     else{
-        //         i++;
-        //     }
-        // }
-        // return nums.length-i;
-        int i = 0, n = nums.length;
-while (i < n) {
-    if (nums[i] == val) {
-        nums[i] = nums[n - 1];
-        n--;
-    } else {
-        i++;
-    }
-}
-return n;
-
+        int i=0;
+        int n=nums.length;
+        while(i<n){
+            if(nums[i]==val){
+                nums[i]=nums[n-1];
+                n--;
+            }
+            else{
+                i++;
+            }
+        }
+        return n;
     }
 }
