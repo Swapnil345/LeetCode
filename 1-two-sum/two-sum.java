ba8fbd44937c496eda8b5fef6824b98f08d369f4
Swapnil Kumar <swapnilkm03@gmail.com>
2025-11-24 00:42:04 +0530

    Time: 45 ms (29.14%) | Memory: 46.3 MB (24.93%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,20 +3,10 @@ class Solution {
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
+                    return new int[] {i,j};
                 }
             }
         }
-        return new int[]{};
-
-        // HashMap<Integer,Integer> map=new HashMap<>();
-        // for(int i=0;i<nums.length;i++){
-        //     int complement=target-nums[i];
-        //     if(map.containsKey(complement)){
-        //         return new int[]{map.get(complement),i};
-        //     }
-        //     map.put(nums[i],i);
-        // }
-        // return new int[]{};
+        return new int[] {-1,-1};
     }
 }
